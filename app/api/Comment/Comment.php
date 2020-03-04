bf5045ec908fc944b3cfa1824ae7d6557e1686a1
VaskillerDev <Vaskoooo9241@gmail.com>
2020-03-04 16:14:02 +0300

    modify with Model

--- app/api/Comment/Comment.php
+++ app/api/Comment/Comment.php
@@ -1,23 +1,23 @@
 <?php
 // global declaration
 namespace Comment;
+use Engine\Model;
+
 function getDomain () {
     return "domain";
 }
-
-class Comment
+//TODO: Describe CRUD - methods for this class
+class Comment extends Model
 {
     public $myField;
 }
-
+/*
 $a = new Comment();
 $a->myField = "text";
 
-
-
 $data = new \Engine\DatabaseInfo();
 $data->table = "table";
-$data->file = "./../../.cache/file.sqlite";
+$data->file = "./../../.cache/file.sqlite";*/
 /*
 $adapter = new \Engine\SQLite3Adapter();
 $adapter->openTable($data,$a);

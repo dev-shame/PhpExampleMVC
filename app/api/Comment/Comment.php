3894e3be4125b5351878a7b7df3f3198cbb1530a
VaskillerDev <Vaskoooo9241@gmail.com>
2020-03-04 16:38:06 +0300

    fix

--- app/api/Comment/Comment.php
+++ app/api/Comment/Comment.php
@@ -2,17 +2,14 @@
 
 // global declaration
 namespace Comment;
-<<<<<<< HEAD
+
 use Engine\Model;
 
-function getDomain () {
-    return "domain";
-=======
 include (__DIR__.'/../../../engine/database.php');
 
  function getDomain () {
     return "comment";
->>>>>>> e6a600c8c715e52dac9b9183ca964a1ca24689c2
+
 }
 //TODO: Describe CRUD - methods for this class
 class Comment extends Model

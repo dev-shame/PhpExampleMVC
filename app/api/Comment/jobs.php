706a5b218de69bc56d8d4ac9959ddcb7581e4da8
VaskillerDev <vaskoooo9241@gmail.com>
2020-03-05 09:34:41 +0300

    refactoring

--- app/api/Comment/jobs.php
+++ app/api/Comment/jobs.php
@@ -1,11 +1,31 @@
 <?php
 
 namespace Comment;
-class Atr {
-    public function test() : bool {
-        echo "hello from test()";
-        return true;
-    }
+
+include_once    ($_SERVER['DOCUMENT_ROOT']."/app/api/Comment/Comment.php");
+
+// GET
+// user, content, likes
+function create() : bool{
+    $req = $_GET;
+    if ($req['user'] === NULL) {http_response_code(400); return false;}
+    $comment = new Comment();
+    $comment->create(
+        $req['user'],
+        $req['content'],
+        $req['likes']
+    );
+    http_response_code(200);
+    return true;
 }
 
-
+// GET
+// user
+function find(){
+    $req = $_GET;
+    $comment = new Comment();
+    $data = $comment->findByUser($req['user']);
+    $json = json_encode($data);
+    echo $json;
+    http_response_code(200);
+}
--- app/api/Comment/router.php
+++ app/api/Comment/router.php
@@ -2,6 +2,23 @@
 
 namespace Comment;
 
-return array(
-    "/test" => new \Engine\Route("get",test(),"/test")
-);
+include_once ($_SERVER['DOCUMENT_ROOT']."/engine/Route.php");
+include_once ($_SERVER['DOCUMENT_ROOT']."/app/api/Comment/jobs.php");
+use Engine\Route;
+
+return [
+    "/create" => new Route(
+        ["GET"],
+        "",
+        [
+            function (){create();},
+        ],
+        ),
+    "/find" => new Route(
+        ["GET"],
+        "",
+        [
+            function (){find();}
+        ],
+        ),
+];

55964c2a4271e790f856f2080a38c0ca3db2fa74
VaskillerDev <vaskoooo9241@gmail.com>
2020-03-08 07:27:29 +0300

    add User listener

--- engine/router.php
+++ engine/router.php
@@ -2,11 +2,14 @@
 namespace Engine;
 use Comment\Comment;
 use Post\Post;
+use User\User;
 
 include_once    ($_SERVER['DOCUMENT_ROOT']."/app/api/Comment/Comment.php");
 include_once    ($_SERVER['DOCUMENT_ROOT']."/app/api/Comment/jobs.php");
 include_once    ($_SERVER['DOCUMENT_ROOT']."/app/api/Post/Post.php");
 include_once    ($_SERVER['DOCUMENT_ROOT']."/app/api/Post/jobs.php");
+include_once    ($_SERVER['DOCUMENT_ROOT']."/app/api/User/User.php");
+include_once    ($_SERVER['DOCUMENT_ROOT']."/app/api/User/jobs.php");
 include_once    ($_SERVER['DOCUMENT_ROOT']."/engine/Log.php");
 
 //get and split url address
@@ -44,16 +47,14 @@ Log::console_log($otherInString);*/
 
 $comment    = new Comment();
 $post       = new Post();
+$user       = new User();
 switch ($domain) {
     case "main":  include_once($_SERVER['DOCUMENT_ROOT']."/app/view/index.html"); break;
     case $comment->getClassName() : $comment->listenRoute($otherInString); break;
-    case $post->getClassName() : $post->listenRoute($otherInString); break;
+    case $post->getClassName() :    $post->listenRoute($otherInString); break;
+    case $user->getClassName() :    $user->listenRoute($otherInString); break;
     default: http_response_code(404);
 }
 
 //$comment = new User();
 //$comment->listenRoute("/find");
-
-
-
-
